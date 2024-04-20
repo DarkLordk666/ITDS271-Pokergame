90f7e05447db424f0b374cc45b6174a33f801055
DarkLordk666 <108443755+DarkLordk666@users.noreply.github.com>
2024-04-20 18:12:59 +0700

    Delete android directory

--- android/app/src/main/kotlin/com/example/pokards/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.tidann.pokards
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/tidann/pokards/pokards/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.tidann.pokards.pokards
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
